cbadf84c5d814f581c0133d4c42d6abd3e564e43
TukaHeba <155295942+TukaHeba@users.noreply.github.com>
2024-06-24 01:03:11 +0300

    update storeImage in UploadImageTrait

--- app/Http/Traits/UploadImageTrait.php
+++ app/Http/Traits/UploadImageTrait.php
@@ -2,18 +2,29 @@
 
 namespace App\Http\Traits;
 
-use Illuminate\Support\Facades\Storage;
+use Exception;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 trait UploadImageTrait
 {
+    // Tuka: Store an image for services
+    // firstly it will check if there is more than one dot in the image name, for security reasons
+    // if yes, it will throw an error and not complete the operation
+    // if no, it will save the image with this format: currentTime_random10letters.png
     public function storeImage($img, $folderName, $disk = 'images')
     {
-        $photo = time().$img->getClientOriginalName();
+        $originalName = $img->getClientOriginalName();
+        if (preg_match('/\.[^.]+\./', $originalName)) {
+            throw new Exception(trans('general.notAllowedAction'), 403);
+        }
+    
+        // $photo =  time() . '_' . pathinfo($originalName, PATHINFO_FILENAME) .'.png';
+        $photo = time() . '_' . Str::random(10) . '.png';
         $path = $img->storeAs($folderName, $photo, $disk);
+        
         return $path;
     }
-
     public function deleteImage($path, $disk ='images')
     {   
         return Storage::disk($disk)->delete($path);
